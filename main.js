d120a23cb4e7f92376ce80a2582e4f750376df84
kandadave <kandachumo@gmail.com>
2025-06-21 14:45:56 +0300

    liker app

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll('.like');
+const modal = document.getElementById('modal')
+const modalMessage = document.getElementById('modal-message')
 
+likeButtons.forEach(button => {
+  const heartGlyph = button.querySelector('.like-glyph');
 
-
-
+  button.addEventListener('click', () => {
+    if(heartGlyph.textContent === EMPTY_HEART){
+      mimicServerCall()
+        .then(() => {
+          heartGlyph.textContent = FULL_HEART;
+          heartGlyph.classList.add('activated-heart')
+        })
+        .catch ((error) => {
+          modal.classList.remove('hidden');
+          modalMessage.textContent = error;
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000)
+        })
+    }else {
+      //User clicks on a full heart
+      heartGlyph.textContent = EMPTY_HEART;
+      heartGlyph.classList.remove('activated-heart')
+    }
+  })
+})
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
